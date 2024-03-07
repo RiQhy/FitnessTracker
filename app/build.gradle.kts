c23077056a5cd22f3d1b220542b20574de455985
Semen Morozov <semenmorozovsuomi@gmail.com>
2024-03-07 19:16:25 +0200

    #dependency update

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
+    id("kotlin-kapt")
     id("com.google.devtools.ksp") version "1.6.20-1.0.5"
 }
 
@@ -62,6 +63,7 @@ dependencies {
     val room_version = "2.6.1"
     implementation("androidx.room:room-runtime:$room_version")
     annotationProcessor("androidx.room:room-compiler:$room_version")
+    kapt ("androidx.room:room-compiler:$room_version")
     ksp("androidx.room:room-compiler:$room_version")
 
     implementation ("androidx.room:room-ktx:$room_version")
