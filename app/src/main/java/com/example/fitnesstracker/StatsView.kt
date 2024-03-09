7a5e09e52b3cbb14b63a243de9701b334a5d9ecf
Emil-Zghaib <emilz@metropolia.fi>
2024-03-09 14:12:46 +0200

    Kalenteri
    Kalenteri toimii

--- app/src/main/java/com/example/fitnesstracker/StatsView.kt
+++ app/src/main/java/com/example/fitnesstracker/StatsView.kt
@@ -54,6 +54,18 @@ import androidx.compose.ui.unit.sp
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 import androidx.navigation.NavController
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material.icons.filled.ArrowForward
+import androidx.compose.material3.*
+import androidx.compose.runtime.*
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.fitnesstracker.ui.theme.FitnessTrackerTheme
+import java.time.LocalDate
+import java.time.format.DateTimeFormatter
+import java.time.format.FormatStyle
 
 
 
@@ -199,6 +211,7 @@ class StatsView : AppCompatActivity(), SensorEventListener {
                         modifier = Modifier.padding(innerPadding),
                     ) {
                         Column {
+                            CalendarApp()
                             HeartRateStuff(MyViewModel())
                             StatsViewStuff(totalSteps.toInt() - previousTotalSteps.toInt())
                         }
@@ -404,4 +417,140 @@ class StatsView : AppCompatActivity(), SensorEventListener {
                     }
                 }
             }
+    @Preview(showSystemUi = true)
+    @Composable
+    fun CalendarAppPreview() {
+        FitnessTrackerTheme {
+            CalendarApp(
+                modifier = Modifier.padding(16.dp)
+            )
         }
+    }
+    @Composable
+    fun CalendarApp(
+        modifier: Modifier = Modifier,
+    ) {
+        val dataSource = CalendarDataSource()
+        var data by remember { mutableStateOf(dataSource.getData(lastSelectedDate = dataSource.today)) }
+        Column(modifier = modifier) {
+            Header(
+                data = data,
+                onPrevClickListener = { startDate ->
+                    val finalStartDate = startDate.minusDays(1)
+                    data = dataSource.getData(startDate = finalStartDate, lastSelectedDate = data.selectedDate.date)
+                },
+                onNextClickListener = { endDate ->
+                    val finalStartDate = endDate.plusDays(2)
+                    data = dataSource.getData(startDate = finalStartDate, lastSelectedDate = data.selectedDate.date)
+                }
+            )
+            Content(data = data) { date ->
+                data = data.copy(
+                    selectedDate = date,
+                    visibleDates = data.visibleDates.map {
+                        it.copy(
+                            isSelected = it.date.isEqual(date.date)
+                        )
+                    }
+                )
+            }
+        }
+    }
+
+    @Composable
+    fun Header(
+        data: CalendarUiModel,
+        onPrevClickListener: (LocalDate) -> Unit,
+        onNextClickListener: (LocalDate) -> Unit,
+    ) {
+        Row {
+            Text(
+                text = if (data.selectedDate.isToday) {
+                    "Today"
+                } else {
+                    data.selectedDate.date.format(
+                        DateTimeFormatter.ofLocalizedDate(FormatStyle.FULL)
+                    )
+                },
+                modifier = Modifier
+                    .weight(1f)
+                    .align(Alignment.CenterVertically)
+            )
+            IconButton(onClick = {
+                onPrevClickListener(data.startDate.date)
+            }) {
+                Icon(
+                    imageVector = Icons.Filled.ArrowBack,
+                    contentDescription = "Back"
+                )
+            }
+            IconButton(onClick = {
+                onNextClickListener(data.endDate.date)
+            }) {
+                Icon(
+                    imageVector = Icons.Filled.ArrowForward,
+                    contentDescription = "Next"
+                )
+            }
+        }
+    }
+
+    @Composable
+    fun Content(
+        data: CalendarUiModel,
+        onDateClickListener: (CalendarUiModel.Date) -> Unit,
+    ) {
+        LazyVerticalGrid(columns = GridCells.Adaptive(minSize = 48.dp)) {
+            items(data.visibleDates.size) { index ->
+                ContentItem(
+                    date = data.visibleDates[index],
+                    onDateClickListener
+                )
+            }
+        }
+    }
+
+    @OptIn(ExperimentalMaterial3Api::class)
+    @Composable
+    fun ContentItem(
+        date: CalendarUiModel.Date,
+        onClickListener: (CalendarUiModel.Date) -> Unit,
+    ) {
+        Card(
+            modifier = Modifier
+                .padding(vertical = 4.dp, horizontal = 4.dp)
+                .clickable {
+                    onClickListener(date)
+                }
+            ,
+            colors = CardDefaults.cardColors(
+                containerColor = if (date.isSelected) {
+                    MaterialTheme.colorScheme.primary
+                } else {
+                    MaterialTheme.colorScheme.secondary
+                }
+            ),
+        ) {
+            Column(
+                modifier = Modifier
+                    .width(40.dp)
+                    .height(48.dp)
+                    .padding(4.dp)
+            ) {
+                Text(
+                    text = date.day,
+                    modifier = Modifier.align(Alignment.CenterHorizontally),
+                    style = MaterialTheme.typography.bodySmall
+                )
+                Text(
+                    text = date.date.dayOfMonth.toString(),
+                    modifier = Modifier.align(Alignment.CenterHorizontally),
+                    style = MaterialTheme.typography.bodyMedium,
+                )
+            }
+        }
+    }
+        }
+
+
+
