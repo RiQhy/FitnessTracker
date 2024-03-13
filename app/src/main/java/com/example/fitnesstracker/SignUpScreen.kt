8eee054adff90d8f0fa35035e65c089d0690bfdd
RiQhy <riku.nokelainen@metropolia.fi>
2024-03-13 21:29:21 +0200

    Fixed error message text

--- app/src/main/java/com/example/fitnesstracker/SignUpScreen.kt
+++ app/src/main/java/com/example/fitnesstracker/SignUpScreen.kt
@@ -1,10 +1,26 @@
 package com.example.fitnesstracker
 
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.text.KeyboardOptions
-import androidx.compose.material3.*
-import androidx.compose.runtime.*
+import androidx.compose.material3.Button
+import androidx.compose.material3.Snackbar
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -179,7 +195,7 @@ fun SignUpScreen(navController: NavController, viewModel: SignUpViewModel) {
                     ) {
                         Text(
                             text = message,
-                            color = Color.White,
+                            color = Color.Black,
                             textAlign = TextAlign.Center,
                             modifier = Modifier.align(Alignment.Center)
                         )
